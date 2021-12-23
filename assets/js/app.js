e297c34a0835298380121b31a2ffd0f94d7a4ac3
manucarenjot <carenjotje@gmail.com>
2021-12-23 14:36:30 +0100

    Quizz jquery

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,31 @@
-let good =$('.good')
+
+let button =$('.button');
+let button2 =$('.button2');
 score = 0;
-$('button').click(function () {
-    if (good.is(':checked')) {
+button.click(function () {
+    if ($('.good').is(':checked')) {
         console.log('is ok');
-       $('#quest1').css('border-color', 'green');
+        button.parent('div').css('border-color', 'green');
+        button.remove();
+        $('#quest2').css('display', 'block');
+        score++
+        console.log(score)
+    }
+    else {
+        $('#quest2').css('display', 'block');
+    }
+})
+
+button2.click(function () {
+    if ($('.good2').is(':checked')) {
+        console.log('is ok');
+        button2.parent('div').css('border-color', 'green');
+        button2.remove();
+        $('#quest3').css('display', 'block');
+        score++
+        console.log(score)
+    }
+    else {
+        $('#quest3').css('display', 'block');
     }
 })
