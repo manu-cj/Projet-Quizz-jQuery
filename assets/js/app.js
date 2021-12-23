d08d22b25a18dee05edf0aa554e3ff1fc7291081
manucarenjot <carenjotje@gmail.com>
2021-12-23 15:46:55 +0100

    Quizz jquery

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,13 +15,13 @@ button.click(function () {
     if ($('.good').is(':checked')) {
         button.parent('div').css('border-color', 'green');
         button.remove();
-        $('#quest2').fadeIn(1500);
+        $('#quest2').css('display', 'flex');
         score++
         console.log(score)
         $('#quest1').fadeOut(1000);
     }
     else {
-        $('#quest2').fadeIn(1500);
+        $('#quest2').css('display', 'flex');
         $('#quest1').fadeOut(1000);
         button.remove();
     }
@@ -31,12 +31,12 @@ button2.click(function () {
     if ($('.good2').is(':checked')) {
         button2.parent('div').css('border-color', 'green');
         button2.remove();
-        $('#quest3').fadeIn(1500);
+        $('#quest3').css('display', 'flex');
         score++
         $('#quest2').fadeOut(1000);
     }
     else {
-        $('#quest3').fadeIn(1500);
+        $('#quest3').css('display', 'flex');
         $('#quest2').fadeOut(1000);
         button2.remove();
     }
@@ -46,12 +46,12 @@ button3.click(function () {
     if ($('.good3').is(':checked')) {
         button3.parent('div').css('border-color', 'green');
         button3.remove();
-        $('#quest4').fadeIn(1500);
+        $('#quest4').css('display', 'flex');
         score++
         $('#quest3').fadeOut(1000);
     }
     else {
-        $('#quest4').fadeIn(1500);
+        $('#quest4').css('display', 'flex');
         button3.remove();
         $('#quest3').fadeOut(1000);
     }
@@ -61,12 +61,12 @@ button4.click(function () {
     if ($('.good4').is(':checked')) {
         button4.parent('div').css('border-color', 'green');
         button4.remove();
-        $('#quest5').fadeIn(1500);
+        $('#quest5').css('display', 'flex');
         score++
         $('#quest4').fadeOut(1000);
     }
     else {
-        $('#quest5').fadeIn(1500);
+        $('#quest5').css('display', 'flex');
         button4.remove();
         $('#quest4').fadeOut(1000);
     }
@@ -76,12 +76,12 @@ button5.click(function () {
     if ($('.good5').is(':checked')) {
         button5.parent('div').css('border-color', 'green');
         button5.remove();
-        $('#quest6').fadeIn(1500);
+        $('#quest6').css('display', 'flex');
         score++
         $('#quest5').fadeOut(1000);
     }
     else {
-        $('#quest6').fadeIn(1500);
+        $('#quest6').css('display', 'flex');
         button5.remove();
         $('#quest5').fadeOut(1000);
     }
@@ -91,12 +91,12 @@ button6.click(function () {
     if ($('.good6').is(':checked')) {
         button6.parent('div').css('border-color', 'green');
         button6.remove();
-        $('#quest7').fadeIn(1500);
+        $('#quest7').css('display', 'flex');
         score++
         $('#quest6').fadeOut(1000);
     }
     else {
-        $('#quest7').fadeIn(1500);
+        $('#quest7').css('display', 'flex');
         button6.remove();
         $('#quest6').fadeOut(1000);
     }
@@ -106,12 +106,12 @@ button7.click(function () {
     if ($('.good7').is(':checked')) {
         button7.parent('div').css('border-color', 'green');
         button7.remove();
-        $('#quest8').fadeIn(1500);
+        $('#quest8').css('display', 'flex');
         score++
         $('#quest7').fadeOut(1000);
     }
     else {
-        $('#quest8').fadeIn(1500);
+        $('#quest8').css('display', 'flex');
         button7.remove();
         $('#quest7').fadeOut(1000);
     }
@@ -121,12 +121,12 @@ button8.click(function () {
     if ($('.good8').is(':checked')) {
         button8.parent('div').css('border-color', 'green');
         button8.remove();
-        $('#quest9').fadeIn(1500);
+        $('#quest9').css('display', 'flex');
         score++
         $('#quest8').fadeOut(1000);
     }
     else {
-        $('#quest9').fadeIn(1500);
+        $('#quest9').css('display', 'flex');
         button8.remove();
         $('#quest8').fadeOut(1000);
     }
@@ -136,12 +136,12 @@ button9.click(function () {
     if ($('.good9').is(':checked')) {
         button9.parent('div').css('border-color', 'green');
         button9.remove();
-        $('#quest10').fadeIn(1500);
+        $('#quest10').css('display', 'flex');
         score++
         $('#quest9').fadeOut(1000);
     }
     else {
-        $('#quest10').fadeIn(1500);
+        $('#quest10').css('display', 'flex');
         button9.remove();
         $('#quest9').fadeOut(1000);
     }
